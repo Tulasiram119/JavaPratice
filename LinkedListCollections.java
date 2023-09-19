2219d50f93ada4e0934985a02d069bd4afc6e2e4
Tulasiram <tulasiramkancharla119@gmail.com>
2023-09-19 15:01:55 +0530

    completed

---
+++ LinkedListCollections.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+public class LinkedListCollections {
+   
+    //find mid
+
+
+    //left & right likedlist
+
+
+    //merge
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addLast(4);
+        ll.addLast(5);        
+        ll.addFirst(7);
+        ll.addFirst(10);
+        System.out.println(ll.size());
+    }
+}
